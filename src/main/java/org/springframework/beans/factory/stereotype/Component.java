8063c4a36d1409cd4480343a62dab1cda826f5ec
Odeyalo <theprogrammerinfuture@gmail.com>
2022-06-20 22:52:23 +0300

    This annotation is scanned by AnnotationConfigScanner and registers in application context

---
+++ src/main/java/org/springframework/beans/factory/stereotype/Component.java
@@ -0,0 +1,15 @@
+package org.springframework.beans.factory.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Component {
+
+    String scope() default "singleton";
+
+    String beanName() default "";
+}
